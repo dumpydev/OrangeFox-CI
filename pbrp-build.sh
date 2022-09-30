91e8b26180af5c6c59f1414551a1d61f786a946c
Hakimi0804 <hakimifirdaus944@gmail.com>
2022-09-30 11:51:00 +0800

    foo
    Signed-off-by: Hakimi0804 <hakimifirdaus944@gmail.com>

--- pbrp-build.sh
+++ pbrp-build.sh
@@ -5,7 +5,7 @@ MANIFEST="https://github.com/PitchBlackRecoveryProject/manifest_pb.git"
 MANIFEST_BRANCH="android-12.1"
 DEVICE="RMX2001"
 DT_LINK="https://github.com/PitchBlackRecoveryProject/android_device_realme_RMX2001-pbrp"
-DT_BRANCH="android-11.0"
+DT_BRANCH="android-11.0-cirrus"
 DT_PATH="device/realme/RMX2001"
 
 git config --global user.email "hakimifirdaus944@gmail.com"
@@ -23,11 +23,12 @@ git clone "$DT_LINK" --depth=1 --single-branch -b "$DT_BRANCH" "$DT_PATH"
 # Add for the unified commit
 mv device/realme/RMX2001/omni_{RM6785,RMX2001}.mk
 sed -i 's/RM6785/RMX2001/g' device/realme/RMX2001/*.mk
+sed -i 's/102760448/67108864/g' device/realme/RMX2001/BoardConfig.mk
 
 . build/envsetup.sh && \
     lunch "omni_$DEVICE-eng" && \
     make -j8 pbrp
-
+ 
 cd "out/target/product/$DEVICE"
 
 curl -sL https://git.io/file-transfer | sh
